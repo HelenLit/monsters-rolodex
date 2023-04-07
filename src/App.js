17cad6e08af3639ab14be5a99fc5abf07688a9ac
Olena Litovska <litovskaolena@gmail.com>
2023-04-07 17:29:12 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import {Component} from "react";
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      name:'Helen'
+    }
+  }
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Hello {this.state.name}!</p>
+            <button>Change name</button>
+          </header>
+        </div>
+    );
+  }
+
+
+}
+
+export default App;
